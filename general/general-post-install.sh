8ce2642801aed6dfffaa2ec944f150e5c01fd327
secretfirefox <141532262+secretfirefox@users.noreply.github.com>
2025-01-02 22:47:45 -0400

    Add files via upload

--- general/general-post-install.sh
+++ general/general-post-install.sh
@@ -1,39 +1,37 @@
 # This script was made by Secret Firefox.
 # This speeds up post installation on your Void Linux.
-# This version is for the user to set their own DE/WM later.
+# This version is for the user to install their own DE/WM later.
 # Enjoy! (Mandatory)
 
 #                                                          #
 # [Steps bellow are for post-installation and basic setup] #
 #                                                          #
 
-# Ensure the system is up-to-date
+# Update the system
 
-sudo xbps-install -Syu
-sudo xbps-install -uy xbps
 sudo xbps-install -Syu
 
 # Add the nonfree repository
 
 sudo xbps-install -Rs void-repo-nonfree -y
 
-# Install dbus, elogind and Network Manager
+# Install dbus, elogind and NetworkManager
 
 sudo xbps-install dbus elogind NetworkManager -y
 
 # Install recommended packages
 
-sudo xbps-install curl wget git xz unzip zip nano vim gptfdisk xtools mtools mlocate ntfs-3g fuse-exfat bash-completion linux-headers gtksourceview4 ffmpeg mesa-vdpau mesa-vaapi htop fastfetch numlockx psmisc 7zip cpupower xmirror mesa-demos noto-fonts-cjk noto-fonts-emoji unrar -y
+sudo xbps-install curl wget git xz unzip zip nano vim gptfdisk xtools mtools mlocate ntfs-3g fuse-exfat bash-completion linux-headers gtksourceview4 ffmpeg mesa-vdpau mesa-vaapi htop fastfetch numlockx psmisc 7zip cpupower xmirror mesa-demos noto-fonts-cjk noto-fonts-emoji xdg-user-dirs xdg-user-dirs-gtk -y
 
-# Install some development packages (optional but recommended)
+# Install development packages
 
 sudo xbps-install autoconf automake bison m4 make libtool flex meson ninja optipng sassc -y
 
-# Install Pipewire and Wireplumber
+# Install sound system (Pipewire and Wireplumber)
 
 sudo xbps-install pipewire wireplumber -y
 
-# Authorize and enable Pipewire and Wireplumber to autostart
+# Authorize and set Pipewire and Wireplumber to autostart
 
 sudo chmod +x /usr/share/applications/pipewire.desktop
 sudo chmod +x /usr/share/applications/pipewire-pulse.desktop
@@ -42,7 +40,7 @@ sudo ln -s /usr/share/applications/pipewire.desktop /etc/xdg/autostart
 sudo ln -s /usr/share/applications/pipewire-pulse.desktop /etc/xdg/autostart
 sudo ln -s /usr/share/applications/wireplumber.desktop /etc/xdg/autostart
 
-# Install cronie and enable its service
+# Install cronie and enable its service 
 
 sudo xbps-install cronie -y
 sudo ln -s /etc/sv/cronie /var/service
@@ -53,13 +51,7 @@ sudo xbps-install socklog-void -y
 sudo ln -s /etc/sv/socklog-unix /var/service
 sudo ln -s /etc/sv/nanoklogd /var/service
 
-# Install chrony to keep time in sync
-
-#sudo xbps-install chrony -y
-#sudo ln -s /etc/sv/chronyd /var/service
-#sudo sv status chronyd
-
-# Install Profile Sync Daemon, to speed up browsers
+# Install Profile Sync Daemon (speeds up browsers)
 
 git clone https://github.com/madand/runit-services
 cd runit-services
@@ -69,7 +61,7 @@ sudo chmod +x /etc/sv/psd/*
 cd ..
 sudo rm -rf runit-services
 
-# Install Firefox and set a better font for it
+# Install Firefox and make its fonts look better (with antialiasing)
 
 sudo xbps-install firefox firefox-i18n-en-US -y
 sudo ln -s /usr/share/fontconfig/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d
@@ -80,19 +72,20 @@ sudo xbps-reconfigure -f fontconfig
 xdg-user-dirs-update
 xdg-user-dirs-gtk-update
 
-# Turn off dhcpcd and wpa_supplicant in favor of Network Manager
+# Switch off dhcpcd and wpa_supplicant in favor of Network Manager 
 
 sudo sv down dhcpcd wpa_supplicant
 sudo touch /etc/sv/dhcpcd/down /etc/sv/wpa_supplicant/down
 sudo sv status dhcpcd
 sudo sv status wpa_supplicant
 
-# Enable dbus, elogind and NetworkManager
+# Enable dbus and NetworkManager
 
 sudo ln -s /etc/sv/dbus /var/service
-sudo ln -s /etc/sv/elogind/ /var/service
 sudo ln -s /etc/sv/NetworkManager /var/service
 
 # Inform finished installation
 
-echo "Installation complete. Reboot your computer, and use nmtui to connect to the internet."
+echo "Installation complete. Please reboot your computer." 
+
+
